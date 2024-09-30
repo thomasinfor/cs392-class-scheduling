432dbaffa0df7fa35bd8dcc121a7f5242356c71d
thomasinfor <thomaswang2003@gmail.com>
2024-09-30 10:32:20 -0500

    Tutorial: Style the app

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,6 +1,17 @@
+import './CourseList.css';
+
 const CourseList = ({ courses }) => {
-  return Object.entries(courses).map(([code, detail]) =>
-    <p key={code}>{detail.term} CS {detail.number}: {detail.title}</p>
+  return (
+    <div className="container">
+      {Object.entries(courses).map(([code, detail]) =>
+        <div className="card" key={code}>
+          <h3 className="code">{detail.term} CS {detail.number}</h3>
+          <div className="title">{detail.title}</div>
+          <hr className="divider"/>
+          <div className="time">{detail.meets}</div>
+        </div>
+      )}
+    </div>
   );
 }
 
